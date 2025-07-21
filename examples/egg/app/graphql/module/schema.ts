9735b419ed038b712ad46ea97f749a3fef5f5e88
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-07-21 20:33:01 +0300

    Fix egg

--- examples/egg/app/graphql/module/schema.ts
+++ examples/egg/app/graphql/module/schema.ts
@@ -1,8 +1,9 @@
+import { Context } from 'egg';
 import { createSchema } from 'graphql-yoga';
 import { testResolver } from './test/resolver';
 import { testTypeDefs } from './test/schema';
 
-export const schema = createSchema({
+export const schema = createSchema<Context>({
   typeDefs: [testTypeDefs],
   resolvers: [testResolver],
 });
