3feefbed4853586f4f93c29e0f9c5a4189457310
Szymon Łągiewka <slagiewka@users.noreply.github.com>
2025-09-19 21:18:59 +0200

    chore: update Deno examples and references
    - docs are hosted under .com
    - Deno.serve stabilised a while back
    - add imports w/o which example scripts wouldn't run
    - stop using `--unstable` as it's been removed in v2 and isn't serving
    any significant purpose in examples

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -1,7 +1,6 @@
-import { serve } from 'https://deno.land/std@0.157.0/http/server.ts';
 import { yoga } from './yoga.ts';
 
-serve(yoga, {
+Deno.serve(yoga, {
   onListen({ hostname, port }) {
     console.log(`Listening on http://${hostname}:${port}${yoga.graphqlEndpoint}`);
   },
--- examples/deno/test.ts
+++ examples/deno/test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.158.0/testing/asserts.ts';
+import { assertEquals } from 'jsr:@std/assert';
 import { yoga } from './yoga.ts';
 
 Deno.test('Deno example test', async () => {
