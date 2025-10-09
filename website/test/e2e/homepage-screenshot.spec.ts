6dbf544cc7e7e25078d2aa3e684772269be72056
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-10-09 20:28:26 +0200

    Assert website renders

--- website/test/e2e/homepage-screenshot.spec.ts
+++ website/test/e2e/homepage-screenshot.spec.ts
@@ -1,10 +1,8 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('homepage above-the-fold renders as expected', async ({ page }, testInfo) => {
-  // await page.goto('/');
-  // await page.waitForLoadState('networkidle');
-  await page.screenshot({
-    path: testInfo.outputPath('homepage-above-the-fold.png'),
-    fullPage: false,
-  });
+  await page.goto('/');
+  await page.waitForLoadState('networkidle');
+  await page.$eval('#devtools-indicator', el => el?.remove());
+  await expect(page).toHaveScreenshot('homepage-above-the-fold.png', { fullPage: false });
 });
