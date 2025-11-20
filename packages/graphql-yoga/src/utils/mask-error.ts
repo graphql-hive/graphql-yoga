b0173f3a1c673467d2c538b58dd32a4f7acd5228
Valentin Cocaud <v.cocaud@gmail.com>
2025-11-20 14:45:59 +0100

    use toJSON to maske coordinates

--- packages/graphql-yoga/src/utils/mask-error.ts
+++ packages/graphql-yoga/src/utils/mask-error.ts
@@ -1,6 +1,20 @@
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 import { createGraphQLError, getSchemaCoordinate } from '@graphql-tools/utils';
 import { isGraphQLError, isOriginalGraphQLError } from '../error.js';
 import { MaskError } from '../types.js';
+
+// We override the `toJSON` function to mask coordinate, because otherwise, it will be entirely
+// masked for plugins after the onExecuteDone phase (which have an impact for telemetry for example)
+function toJsonWithoutCoordinate(this: GraphQLError): GraphQLFormattedError {
+  const toJSON: typeof GraphQLError.prototype.toJSON =
+    (this as { _originalToJSON?: typeof GraphQLError.prototype.toJSON })._originalToJSON ??
+    GraphQLError.prototype.toJSON;
+  const json = toJSON.apply(this);
+  // @ts-expect-error coordinate is readonly
+  delete json.coordinate;
+
+  return json;
+}
 
 function serializeError(error: unknown) {
   if (isGraphQLError(error)) {
@@ -22,6 +36,12 @@ export const maskError: MaskError = (
   isDev = globalThis.process?.env?.['NODE_ENV'] === 'development',
 ) => {
   if (isOriginalGraphQLError(error)) {
+    if (!isDev) {
+      Object.defineProperties(error, {
+        toJSON: { value: toJsonWithoutCoordinate },
+        _originalToJSON: { value: error.toJSON },
+      });
+    }
     return error;
   }
   const errorExtensions: Record<string, unknown> = {
@@ -36,18 +56,31 @@ export const maskError: MaskError = (
     errorOptions.source = error.source;
     errorOptions.positions = error.positions;
     errorOptions.path = error.path;
+    errorOptions.coordinate = getSchemaCoordinate(error);
     if (isDev && error.originalError) {
       errorExtensions['originalError'] = serializeError(error.originalError);
     }
     if (error.extensions?.['http']) {
       errorExtensions['http'] = error.extensions['http'];
     }
-    if (isDev) {
-      errorOptions.coordinate = getSchemaCoordinate(error);
-    }
   } else if (isDev) {
     errorExtensions['originalError'] = serializeError(error);
   }
 
-  return createGraphQLError(message, errorOptions);
+  const maskedError = createGraphQLError(message, errorOptions);
+
+  if (!isDev) {
+    Object.defineProperties(maskedError, {
+      toJSON: { value: toJsonWithoutCoordinate },
+      _originalToJSON: { value: maskedError.toJSON },
+    });
+    if (maskedError.extensions['originalError'] instanceof GraphQLError) {
+      Object.defineProperties(maskedError.extensions['originalError'], {
+        toJSON: { value: toJsonWithoutCoordinate },
+        _originalToJSON: { value: maskedError.extensions['originalError'].toJSON },
+      });
+    }
+  }
+
+  return maskedError;
 };
