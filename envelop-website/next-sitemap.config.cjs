50e6bae5ca2431c11d0d0eaff6fd37a40b97fdd7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-02-21 09:01:12 -0500

    Move Envelop website (#4347)
    * Move Envelop website
    
    * Ignore eslint
    
    * No lint
    
    * More
    
    * Lets go
    
    * Force
    
    * Try again
    
    * Ignore envelop website
    
    * Lets go
    
    * Ok

---
+++ envelop-website/next-sitemap.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://the-guild.dev/graphql/envelop',
+  generateIndexSitemap: false,
+  output: 'export',
+};
