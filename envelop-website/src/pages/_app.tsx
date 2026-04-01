7d070812bd963037fee14ec2fe5d747b2ff2e8c2
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-04-01 13:04:48 +0300

    No need

--- envelop-website/src/pages/_app.tsx
+++ envelop-website/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-//@ts-expect-error - no types for this package
 import '@theguild/components/style.css';
 
 export default function App({ Component, pageProps }: AppProps) {
