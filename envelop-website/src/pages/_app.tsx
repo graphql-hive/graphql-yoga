a8121d214f7d3abd3535cfe13639e0a673f25ffb
Laurin Quast <laurinquast@googlemail.com>
2026-08-20 13:55:30 +0200

    fix envelop website

--- envelop-website/src/pages/_app.tsx
+++ envelop-website/src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from 'next/app';
 import '@theguild/components/style.css';
+import Head from 'next/head';
+import favicon from '../../public/favicon.ico';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href={favicon.src} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
